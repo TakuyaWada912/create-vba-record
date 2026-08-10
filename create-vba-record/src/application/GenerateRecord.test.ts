1114cb68b4ca013603b8b6228aa70c606b4d094d
Takuya Wada <takuya.wada.0912@gmail.com>
2026-08-10 14:00:43 +0000

    一番簡単なユースケースのみ実装

---
+++ create-vba-record/src/application/GenerateRecord.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, it } from 'vitest';
+import { GenerateRecord, type GenerateRecordCommand } from './GenerateRecord';
+
+describe("GenerateRecord", () => {
+    it("一つのフィールドのレコードクラス文字列が生成される(Getter,Setterあり)", () => {
+        const sut = new GenerateRecord();
+
+        const generateRecordCommand: GenerateRecordCommand = {
+            fieldName: "Id",
+            fieldType: 'String'
+        }
+
+        const result = sut.execute(Array(generateRecordCommand));
+
+        expect(result).toBe(
+            [
+                'Option Explicit',
+                '',
+                "' === fields ===",
+                'Private pId As String',
+                '',
+                "' === getters ===",
+                'Property Get Id() As String',
+                '  Id = pId',
+                'End Property',
+                '',
+            ].join('\n')
+        );
+    })
+})
