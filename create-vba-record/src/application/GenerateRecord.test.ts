92fd73219bd7245e9f1f7805f87f6de6a00fd038
Takuya Wada <takuya.wada.0912@gmail.com>
2026-08-10 14:06:17 +0000

    formatterの適用

--- create-vba-record/src/application/GenerateRecord.test.ts
+++ create-vba-record/src/application/GenerateRecord.test.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {describe, expect, it} from 'vitest';
 import {GenerateRecord, type GenerateRecordCommand} from './GenerateRecord';
 
@@ -29,35 +28,3 @@ describe('GenerateRecord', () => {
     );
   });
 });
-=======
-import { describe, expect, it } from 'vitest';
-import { GenerateRecord, type GenerateRecordCommand } from './GenerateRecord';
-
-describe("GenerateRecord", () => {
-    it("一つのフィールドのレコードクラス文字列が生成される(Getter,Setterあり)", () => {
-        const sut = new GenerateRecord();
-
-        const generateRecordCommand: GenerateRecordCommand = {
-            fieldName: "Id",
-            fieldType: 'String'
-        }
-
-        const result = sut.execute(Array(generateRecordCommand));
-
-        expect(result).toBe(
-            [
-                'Option Explicit',
-                '',
-                "' === fields ===",
-                'Private pId As String',
-                '',
-                "' === getters ===",
-                'Property Get Id() As String',
-                '  Id = pId',
-                'End Property',
-                '',
-            ].join('\n')
-        );
-    })
-})
->>>>>>> 1114cb68b4ca013603b8b6228aa70c606b4d094d
--- create-vba-record/src/application/GenerateRecord.ts
+++ create-vba-record/src/application/GenerateRecord.ts
@@ -1,5 +1,4 @@
 export class GenerateRecord {
-<<<<<<< HEAD
   execute(command: GenerateRecordCommand[]): string {
     const fields = command.map((c) => {
       return `Private p${c.fieldName} As ${c.fieldType}`;
@@ -30,36 +29,3 @@ export interface GenerateRecordCommand {
   fieldName: string;
   fieldType: string;
 }
-=======
-    execute(command: GenerateRecordCommand[]): string {
-
-        const fields = command.map((c) => {
-            return `Private p${c.fieldName} As ${c.fieldType}`
-        })
-
-        const getters = command.map((c) => {
-            return [
-                `Property Get ${c.fieldName}() As ${c.fieldType}`,
-                `  ${c.fieldName} = p${c.fieldName}`,
-                `End Property`
-            ].join('\n');
-        })
-
-        return [
-            'Option Explicit',
-            '',
-            "' === fields ===",
-            ...fields,
-            '',
-            "' === getters ===",
-            ...getters,
-            '',
-        ].join('\n');
-    }
-}
-
-export interface GenerateRecordCommand {
-    fieldName: string
-    fieldType: string
-}
->>>>>>> 1114cb68b4ca013603b8b6228aa70c606b4d094d
