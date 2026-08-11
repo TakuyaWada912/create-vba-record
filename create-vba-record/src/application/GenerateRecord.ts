dc1abbcbb3c5877cc0d96c8fceff17712d61bb3e
Takuya Wada <takuya.wada.0912@gmail.com>
2026-08-11 11:42:07 +0000

    GeneratRecordクラスをリファクタリング

--- create-vba-record/src/application/GenerateRecord.ts
+++ create-vba-record/src/application/GenerateRecord.ts
@@ -1,29 +1,39 @@
 export class GenerateRecord {
   execute(command: GenerateRecordCommand[]): string {
-    const fields = command.map((c) => {
-      return `Private p${c.fieldName} As ${c.fieldType}`;
-    });
-
-    const getters = command.map((c) => {
-      return [
-        `Property Get ${c.fieldName}() As ${c.fieldType}`,
-        `  ${c.fieldName} = p${c.fieldName}`,
-        `End Property`,
-        '',
-      ].join('\n');
-    });
-
-    return [
-      'Option Explicit',
-      '',
-      "' === fields ===",
-      ...fields,
-      '',
-      "' === getters ===",
-      ...getters,
-    ].join('\n');
+    const fields = createFieldsStr(command);
+    const getters = createGettersStr(command);
+    return createRecordStr(fields, getters);
   }
 }
+
+const createFieldsStr = (command: GenerateRecordCommand[]): string[] => {
+  return command.map((c) => {
+    return `Private p${c.fieldName} As ${c.fieldType}`;
+  });
+};
+
+const createGettersStr = (command: GenerateRecordCommand[]): string[] => {
+  return command.map((c) => {
+    return [
+      `Property Get ${c.fieldName}() As ${c.fieldType}`,
+      `  ${c.fieldName} = p${c.fieldName}`,
+      `End Property`,
+      '',
+    ].join('\n');
+  });
+};
+
+const createRecordStr = (fields: string[], getters: string[]): string => {
+  return [
+    'Option Explicit',
+    '',
+    "' === fields ===",
+    ...fields,
+    '',
+    "' === getters ===",
+    ...getters,
+  ].join('\n');
+};
 
 export interface GenerateRecordCommand {
   fieldName: string;
