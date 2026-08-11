615bbf000d0f7f148fce85e9329dbe4839c07570
Takuya Wada <takuya.wada.0912@gmail.com>
2026-08-11 13:04:16 +0000

    リファクタリング

--- create-vba-record/src/application/GenerateRecord.test.ts
+++ create-vba-record/src/application/GenerateRecord.test.ts
@@ -1,16 +1,24 @@
 import {describe, expect, it} from 'vitest';
-import {GenerateRecord, type GenerateRecordCommand} from './GenerateRecord';
+import {
+  GenerateRecord,
+  type GenerateRecordCommand,
+  type GenerateRecordField,
+} from './GenerateRecord';
 
 describe('GenerateRecord', () => {
   const sut = new GenerateRecord();
 
   it('一つのフィールドのレコードクラス文字列が生成される', () => {
+    const generateRecordField: GenerateRecordField = {
+      name: 'Id',
+      type: 'String',
+    };
     const generateRecordCommand: GenerateRecordCommand = {
-      fieldName: 'Id',
-      fieldType: 'String',
+      className: 'User',
+      fields: [generateRecordField],
     };
 
-    const result = sut.execute([generateRecordCommand]);
+    const result = sut.execute(generateRecordCommand);
 
     expect(result).toBe(
       [
@@ -29,22 +37,45 @@ describe('GenerateRecord', () => {
         '  Id = pId',
         'End Property',
         '',
+        "' === Equals ===",
+        'Public Function Equals(ByVal other As Object) As Boolean',
+        '  If other Is Nothing Then',
+        '    Equals = False',
+        '    Exit Function',
+        '  End If',
+        '',
+        '  If TypeName(other) <> "User" Then',
+        '    Equals = False',
+        '    Exit Function',
+        '  End If',
+        '',
+        '  If (Id = other.Id) Then',
+        '    Equals = True',
+        '  Else',
+        '    Equals = False',
+        '  End If',
+        'End Function',
       ].join('\n'),
     );
   });
 
   it('二つのフィールドのレコードクラス文字列が生成される', () => {
-    const c1: GenerateRecordCommand = {
-      fieldName: 'Id',
-      fieldType: 'String',
+    const f1: GenerateRecordField = {
+      name: 'Id',
+      type: 'String',
     };
 
-    const c2: GenerateRecordCommand = {
-      fieldName: 'Amount',
-      fieldType: 'Long',
+    const f2: GenerateRecordField = {
+      name: 'Amount',
+      type: 'Long',
     };
 
-    const result = sut.execute([c1, c2]);
+    const command: GenerateRecordCommand = {
+      className: 'Transaction',
+      fields: [f1, f2],
+    };
+
+    const result = sut.execute(command);
 
     expect(result).toBe(
       [
@@ -69,6 +100,25 @@ describe('GenerateRecord', () => {
         '  Amount = pAmount',
         'End Property',
         '',
+        "' === Equals ===",
+        'Public Function Equals(ByVal other As Object) As Boolean',
+        '  If other Is Nothing Then',
+        '    Equals = False',
+        '    Exit Function',
+        '  End If',
+        '',
+        '  If TypeName(other) <> "Transaction" Then',
+        '    Equals = False',
+        '    Exit Function',
+        '  End If',
+        '',
+        '  If (Id = other.Id) And _',
+        '     (Amount = other.Amount) Then',
+        '    Equals = True',
+        '  Else',
+        '    Equals = False',
+        '  End If',
+        'End Function',
       ].join('\n'),
     );
   });
--- create-vba-record/src/application/GenerateRecord.ts
+++ create-vba-record/src/application/GenerateRecord.ts
@@ -1,47 +1,102 @@
 export class GenerateRecord {
-  execute(command: GenerateRecordCommand[]): string {
-    const fields = createFieldsStr(command);
-    const getters = createGettersStr(command);
-    const constructor = createConstructor(command);
-    return createRecordStr(fields, getters, constructor);
+  execute(command: GenerateRecordCommand): string {
+    const fields = createFieldsStr(command.fields);
+    const getters = createGettersStr(command.fields);
+    const constructor = createConstructor(command.fields);
+    const equals = createEquals(command);
+    return createRecordStr(fields, getters, constructor, equals);
   }
 }
 
-const createFieldsStr = (command: GenerateRecordCommand[]): string[] => {
-  return command.map((c) => {
-    return `Private p${c.fieldName} As ${c.fieldType}`;
+const createFieldsStr = (fields: GenerateRecordField[]): string[] => {
+  return fields.map((f) => {
+    return `Private p${f.name} As ${f.type}`;
   });
 };
 
-const createGettersStr = (command: GenerateRecordCommand[]): string[] => {
-  return command.map((c) => {
+const createGettersStr = (fields: GenerateRecordField[]): string[] => {
+  return fields.map((f) => {
     return [
-      `Property Get ${c.fieldName}() As ${c.fieldType}`,
-      `  ${c.fieldName} = p${c.fieldName}`,
+      `Property Get ${f.name}() As ${f.type}`,
+      `  ${f.name} = p${f.name}`,
       `End Property`,
       '',
     ].join('\n');
   });
 };
 
-const createConstructor = (command: GenerateRecordCommand[]): string[] => {
-  const argsStr = command.map((c) => {
-    const argStr = c.fieldName.toLowerCase() + '_val';
-    return `ByVal ${argStr} As ${c.fieldType}`;
+const createConstructor = (fields: GenerateRecordField[]): string[] => {
+  const signature = createConstructorSignature(fields);
+
+  const assignFields = fields.map((c) => {
+    return `  ${c.name} = ${c.name.toLowerCase() + '_val'}`;
   });
 
-  const signature = `Public Sub Init(${argsStr.join(', ')})`;
-
-  const fields = command.map((c) => {
-    return `  ${c.fieldName} = ${c.fieldName.toLowerCase() + '_val'}`;
-  });
-
-  return [signature, ...fields, 'End Sub'];
+  return [signature, ...assignFields, 'End Sub'];
 };
+
+const createConstructorSignature = (fields: GenerateRecordField[]): string => {
+  const argsStr = fields.map((f) => {
+    const argStr = f.name.toLowerCase() + '_val';
+    return `ByVal ${argStr} As ${f.type}`;
+  });
+
+  return `Public Sub Init(${argsStr.join(', ')})`;
+}
+
+const createEquals = (command: GenerateRecordCommand): string[] => {
+  const fieldCriteria = createFieldCriteria(command.fields);
+
+  return [
+    'Public Function Equals(ByVal other As Object) As Boolean',
+    '  If other Is Nothing Then',
+    '    Equals = False',
+    '    Exit Function',
+    '  End If',
+    '',
+    `  If TypeName(other) <> "${command.className}" Then`,
+    '    Equals = False',
+    '    Exit Function',
+    '  End If',
+    '',
+    ...fieldCriteria,
+    '    Equals = True',
+    '  Else',
+    '    Equals = False',
+    '  End If',
+    'End Function',
+  ];
+};
+
+const createFieldCriteria = (fields: GenerateRecordField[]): string[] => {
+  let result: string[];
+
+  if (fields.length > 1) {
+    result = fields.map((field, index) => {
+      if (index === 0) {
+        return `  If (${field.name} = other.${field.name}) And _`;
+      }
+
+      if (index === fields.length - 1) {
+        return `     (${field.name} = other.${field.name}) Then`;
+      }
+
+      return ``;
+    });
+  } else {
+    result = fields.map((f) => {
+      return `  If (${f.name} = other.${f.name}) Then`;
+    });
+  }
+
+  return result;
+}
+
 const createRecordStr = (
   fields: string[],
   getters: string[],
   constructor: string[],
+  equals: string[],
 ): string => {
   return [
     'Option Explicit',
@@ -54,10 +109,17 @@ const createRecordStr = (
     '',
     "' === Getters ===",
     ...getters,
+    "' === Equals ===",
+    ...equals,
   ].join('\n');
 };
 
 export interface GenerateRecordCommand {
-  fieldName: string;
-  fieldType: string;
+  className: string;
+  fields: GenerateRecordField[];
+}
+
+export interface GenerateRecordField {
+  name: string;
+  type: string;
 }
